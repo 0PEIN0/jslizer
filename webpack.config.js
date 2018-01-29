961f7e03b0639b3403899f4d9bcf8ae9300099d0
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-01-30 01:29:09 +0600

    Generated min jslizer file along with unmified file.
    Signed-off-by: S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const path = require('path');
 const webpack = require('webpack');
 
@@ -5,40 +7,18 @@ const srcDir = path.resolve(__dirname, 'src')
 const distDir = path.resolve(__dirname, 'dist')
 const env = process.env.WEBPACK_ENV;
 
-const filename = 'jslizer' + ((env === 'production') ? '.min' : '') + '.js';
-
-const config = {
-    entry: './src/index.js',
-    devtool: 'source-map',
-    output: {
-        filename: filename,
-        path: distDir,
-        publicPath: '/',
-        sourceMapFilename: 'jslizer.map',
-        library: 'jslizer',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015']
-            },
-            exclude: path.resolve(__dirname, "./node_modules"),
-            include: srcDir
-        }]
-    },
-    plugins: [
+function loadDefaultPlugins() {
+    var plugins = [];
+    plugins.push(
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(env)
         })
-    ]
-};
+    );
+    return plugins;
+}
 
-if (env === 'production') {
-    config.plugins.push(
+function loadProductionPlugins(plugins) {
+    plugins.push(
         new webpack.optimize.UglifyJsPlugin({
             compressor: {
                 pure_getters: true,
@@ -47,7 +27,43 @@ if (env === 'production') {
                 warnings: false
             }
         })
-    )
+    );
+    return plugins;
 }
 
-module.exports = config;
+function getDefaultConfig() {
+    return {
+        entry: './src/index.js',
+        devtool: 'source-map',
+        output: {
+            filename: 'jslizer.js',
+            path: distDir,
+            publicPath: '/',
+            sourceMapFilename: 'jslizer.map',
+            library: 'jslizer',
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        },
+        module: {
+            loaders: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                exclude: path.resolve(__dirname, "./node_modules"),
+                include: srcDir
+            }]
+        }
+    };
+}
+
+const config = getDefaultConfig();
+var minConfig = getDefaultConfig();
+minConfig.output.filename = 'jslizer.min.js';
+minConfig.output.sourceMapFilename = 'jslizer.min.map';
+config.plugins = loadDefaultPlugins();
+minConfig.plugins = loadDefaultPlugins();
+minConfig.plugins = loadProductionPlugins(minConfig.plugins);
+
+module.exports = [config, minConfig];
