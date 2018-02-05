36d2d45aea6977a737be603725c56e927f30c2c6
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-02-05 10:48:28 +0600

    Updated system settings variables.
    Signed-off-by: S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>

--- src/core/system-settings.js
+++ src/core/system-settings.js
@@ -8,6 +8,14 @@ class SystemSettings {
         this.SYSTEM_LANGUAGE = 'en';
         this.SYSTEM_DATE_FORMAT = 'DD-MM-YYYY';
         this.SYSTEM_DATE_TIME_FORMAT = 'DD-MM-YYYY hh:mm';
+        this.SYSTEM_DEFAULT_AUTHENTICATION_REQUIRED_VALUE = true;
+        this.SYSTEM_DEFAULT_AUTHORIZATION_HEADER_KEY = 'Authorization';
+        this.SYSTEM_DEFAULT_AUTHORIZATION_HEADER_VALUE_PREFIX = 'JWT ';
+        this.SYSTEM_DEFAULT_ERROR_FIELD_KEY = 'errorMessage';
+        this.SYSTEM_DEFAULT_SERVICE_DATA_OBJECT_KEY = 'data';
+        this.SYSTEM_DEFAULT_API_ERROR_MESSAGE_ID = 'core_system_settings_2';
+        this.LOCAL_API_SERVER_DOMAIN_LIST = ['localhost'];
+        this.LOCAL_API_SERVER_ADDRESS_LIST = ['http://localhost/'];
         this.setEnvironmentRelatedValues();
     }
 
