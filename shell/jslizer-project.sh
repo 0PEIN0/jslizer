1fb9c651d6bf8c0b0edd876a3e8df665dddc8f6e
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-02-15 01:19:22 +0600

    Updated aliases for jslizer project.
    Signed-off-by: S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>

--- shell/jslizer-project.sh
+++ shell/jslizer-project.sh
@@ -1,2 +1,3 @@
-alias jslizer_build="cd $SYSTEM_ROOT_GIT_REPO_FOLDER/jslizer/ && npm_build"
+alias jslizer_build="cd $SYSTEM_ROOT_GIT_REPO_FOLDER/jslizer/ && npm install && npm_build"
 alias jslizer_publish="cd $SYSTEM_ROOT_GIT_REPO_FOLDER/jslizer/ && npm_publish"
+alias jslizer_demo_build="cd $SYSTEM_ROOT_GIT_REPO_FOLDER/jslizer-demo/ && npm install && npm update"
