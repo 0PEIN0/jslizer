af91f094e7c785b19f13fae754460a43537e645a
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-04-25 18:50:49 +0600

    Updated react index file.

--- src/react/index.js
+++ src/react/index.js
@@ -1,5 +1,5 @@
 let reactCoreFactory = (React, jslizer, PROJECT_MESSAGES = {}, PROJECT_SYSTEM_SETTINGS = {}, loader = null) => {
-    jslizer.coreFactory = new jslizer.CoreFactory();
+    jslizer.coreFactory = new jslizer.CoreFactory(PROJECT_SYSTEM_SETTINGS);
     jslizer.coreFactory.defaultReactController = new jslizer.DefaultReactController(loader);
     jslizer.CoreFactory.errorMessage.loadProjectMessages(PROJECT_MESSAGES);
     jslizer.CoreFactory.systemSettings.loadProjectLocalSettings(jslizer.CoreFactory.systemSettings, PROJECT_SYSTEM_SETTINGS);
