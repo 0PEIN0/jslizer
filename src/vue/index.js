895fcbe7ffe28323420e3ffc3ea91696f3e9edd5
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-06-24 11:33:10 +0600

    Added null check in vue.

--- src/vue/index.js
+++ src/vue/index.js
@@ -1,22 +1,26 @@
 let vueCoreFactory = (Vue, jslizer, PROJECT_MESSAGES = {}, PROJECT_SYSTEM_SETTINGS = {}, loader = null) => {
-    jslizer.coreFactory = new jslizer.CoreFactory(PROJECT_SYSTEM_SETTINGS);
-    Vue.use(jslizer)
-    Vue.mixin({
-        // eslint-disable-next-line
-        beforeCreate() {
-            const options = this.$options
-            if (options.jslizer) {
-                this.$coreFactory = options.jslizer.coreFactory
-            } else if (options.parent && options.parent.$coreFactory) {
-                this.$coreFactory = options.parent.$coreFactory
+    if (jslizer != null && jslizer != undefined) {
+        jslizer.coreFactory = new jslizer.CoreFactory(PROJECT_SYSTEM_SETTINGS);
+        Vue.use(jslizer)
+        Vue.mixin({
+            // eslint-disable-next-line
+            beforeCreate() {
+                const options = this.$options
+                if (options.jslizer) {
+                    this.$coreFactory = options.jslizer.coreFactory
+                } else if (options.parent && options.parent.$coreFactory) {
+                    this.$coreFactory = options.parent.$coreFactory
+                }
+                if (this.$coreFactory != null && this.$coreFactory != undefined) {
+                    if (this.$coreFactory.objectHelper.isNull(this.$coreFactory.defaultVueController)) {
+                        this.$coreFactory.defaultVueController = new jslizer.DefaultVueController(loader)
+                    }
+                }
             }
-            if (this.$coreFactory.objectHelper.isNull(this.$coreFactory.defaultVueController)) {
-                this.$coreFactory.defaultVueController = new jslizer.DefaultVueController(loader)
-            }
-        }
-    })
-    jslizer.CoreFactory.errorMessage.loadProjectMessages(PROJECT_MESSAGES)
-    jslizer.CoreFactory.systemSettings.loadProjectLocalSettings(jslizer.CoreFactory.systemSettings, PROJECT_SYSTEM_SETTINGS)
+        })
+        jslizer.CoreFactory.errorMessage.loadProjectMessages(PROJECT_MESSAGES)
+        jslizer.CoreFactory.systemSettings.loadProjectLocalSettings(jslizer.CoreFactory.systemSettings, PROJECT_SYSTEM_SETTINGS)
+    }
 };
 
 
