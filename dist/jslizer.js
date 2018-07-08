af5673b3827d470f8b93107114b7ddb14a7c16a6
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-07-08 15:09:18 +0600

    Added bengali error message binding support.

--- dist/jslizer.js
+++ dist/jslizer.js
@@ -32898,67 +32898,83 @@ Object.defineProperty(exports, "__esModule", {
 var CORE_ERROR_MESSAGES = {
     'core_js_lizer_executor_1': {
         'en': 'Not enough characters. Expecting at least {{minLength}} characters.',
-        'nl': 'Niet genoeg tekens. Verwacht ten minste {{minLength}} tekens.'
+        'nl': 'Niet genoeg tekens. Verwacht ten minste {{minLength}} tekens.',
+        'bn': 'যথেষ্ট অক্ষর নেই অন্তত {{minLength}} অক্ষরগুলি আশা করা হচ্ছে।'
     },
     'core_js_lizer_executor_2': {
         'en': 'Too many characters. Expecting at most {{maxLength}} characters.',
-        'nl': 'Te veel karakters. Verwacht maximaal {{maxLength}} tekens.'
+        'nl': 'Te veel karakters. Verwacht maximaal {{maxLength}} tekens.',
+        'bn': 'অনেক অক্ষর অধিকাংশ {{maxLength}} অক্ষরগুলিতে আশা করা হচ্ছে।'
     },
     'core_js_lizer_executor_3': {
         'en': 'Required.',
-        'nl': 'Verplicht.'
+        'nl': 'Verplicht.',
+        'bn': 'আবশ্যক।'
     },
     'core_js_lizer_executor_4': {
         'en': 'Invalid object type.',
-        'nl': 'Ongeldig objecttype.'
+        'nl': 'Ongeldig objecttype.',
+        'bn': 'অবৈধ বস্তুর ধরন।'
     },
     'core_js_lizer_executor_5': {
         'en': 'Exception occurred. Details: {{exceptionMessage}}',
-        'nl': 'Uitzondering opgetreden. Details: {{exceptionMessage}}'
+        'nl': 'Uitzondering opgetreden. Details: {{exceptionMessage}}',
+        'bn': 'ব্যতিক্রম ঘটেছে বিবরণ: {{exceptionMessage}}।'
     },
     'core_js_lizer_executor_6': {
         'en': 'Null value is not allowed on this field.',
-        'nl': 'Null-waarde is niet toegestaan in dit veld.'
+        'nl': 'Null-waarde is niet toegestaan in dit veld.',
+        'bn': 'এই ক্ষেত্রটিতে নাল মান অনুমোদিত নয়।'
     },
     'core_js_lizer_executor_7': {
         'en': 'Can not supply empty value here.',
-        'nl': 'Kan hier geen lege waarde leveren.'
+        'nl': 'Kan hier geen lege waarde leveren.',
+        'bn': 'এখানে খালি মান সরবরাহ করতে পারে না।'
     },
     'core_js_lizer_executor_8': {
         'en': 'Invalid integer value.',
-        'nl': 'Ongeldige integerwaarde.'
+        'nl': 'Ongeldige integerwaarde.',
+        'bn': 'অবৈধ পূর্ণসংখ্যা মান।'
     },
     'core_js_lizer_executor_9': {
         'en': 'Invalid decimal value.',
-        'nl': 'Ongeldige decimale waarde.'
+        'nl': 'Ongeldige decimale waarde.',
+        'bn': 'অবৈধ দশমিক মান।'
     },
     'core_js_lizer_executor_10': {
         'en': 'Invalid boolean value.',
-        'nl': 'Ongeldige booleaanse waarde.'
+        'nl': 'Ongeldige booleaanse waarde.',
+        'bn': 'অবৈধ বোুলিয়ান মান।'
     },
     'core_base_page_1': {
         'en': 'Callback function can not null as parameter.',
-        'nl': 'Callback-functie kan niet null zijn als parameter.'
+        'nl': 'Callback-functie kan niet null zijn als parameter.',
+        'bn': 'কলব্যাক ফাংশন প্যারামিটার হিসাবে নাল করতে পারে না।'
     },
     'core_base_page_2': {
         'en': 'Page service must be assigned in page constructor.',
-        'nl': 'Paginaservice moet worden toegewezen in paginaconstructor.'
+        'nl': 'Paginaservice moet worden toegewezen in paginaconstructor.',
+        'bn': 'পৃষ্ঠা পরিষেবা পৃষ্ঠার কন্সট্রকটরে নিয়োগ করা আবশ্যক।'
     },
     'core_system_settings_1': {
         'en': 'Could not generate base api url. Possibly the domain you are using is different from what is configured from the server.',
-        'nl': 'Kon basis api-URL niet genereren. Mogelijk is het domein dat u gebruikt anders dan wat van de server is geconfigureerd.'
+        'nl': 'Kon basis api-URL niet genereren. Mogelijk is het domein dat u gebruikt anders dan wat van de server is geconfigureerd.',
+        'bn': 'বেস api url তৈরি করা যায়নি সম্ভবত যে ডোমেনটি আপনি ব্যবহার করছেন সেটি সার্ভার থেকে কনফিগার করা থেকে ভিন্ন।'
     },
     'core_system_settings_2': {
         'en': 'Did not provide error message.',
-        'nl': 'Geen foutmelding gegeven.'
+        'nl': 'Geen foutmelding gegeven.',
+        'bn': 'ত্রুটি বার্তা প্রদান করা হয়নি।'
     },
     'core_base_angular_ctrl_1': {
         'en': '`this.scope = $scope;` is needed in child controller constructor.',
-        'nl': '`this.scope = $scope;` is needed in child controller constructor.'
+        'nl': '`this.scope = $scope;` is needed in child controller constructor.',
+        'bn': '`this.scope = $scope;` সন্তানের নিয়ন্ত্রক কনস্ট্রাক্টর প্রয়োজন হয়।'
     },
     'core_base_ctrl_1': {
         'en': 'Custom error id could not be found, did you forget to declare it? Error ID: {{errorId}}.',
-        'nl': 'Aangepaste fout ID kon niet worden gevonden, ben je vergeten om het te declareren? Foutcode: {{errorId}}.'
+        'nl': 'Aangepaste fout ID kon niet worden gevonden, ben je vergeten om het te declareren? Foutcode: {{errorId}}.',
+        'bn': 'কাস্টম ত্রুটি আইডি পাওয়া যায়নি, আপনি এটি ঘোষণা করতে ভুলবেন না? ত্রুটি আইডি: {{errorId}}।'
     }
 };
 
